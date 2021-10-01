ed18c4f77ca7537e67ef5852bbc5def484972bc3
nlinkous4 <91706133+nlinkous4@users.noreply.github.com>
2021-10-01 12:15:15 -0400

    Updated - Incomplete
    This is the HW3 file I created to fix my original work. It is incomplete

---
+++ script.js
@@ -0,0 +1,8 @@
+function computeForce(){
+    var m, a, force;
+    m = Number.document.form1.getElementById("getMass");
+    a = Number.document.form1.getElementById("getAcceleration");
+    force = (m*a)
+
+    return force;
+}
